c1a62aeb1d13110083231ca2aec1c92526af048c
bearbau <beatrizmarie.bautista@gmail.com>
2026-06-17 09:52:58 +0800

    home page

--- bearbau-website/src/main.ts
+++ bearbau-website/src/main.ts
@@ -1,6 +1,43 @@
 import './style.css'
 import placeholder from './assets/placeholder.jpg'
 
+// card contents
+interface CardData {
+  image: string
+  title: string
+  subtitle: string
+  buttonText: string
+}
+
+// certifications
+const certifications: CardData[] = [
+  { image: placeholder, title: 'Card 1', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 2', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 3', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 4', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 5', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'}
+]
+
+// UI projects
+const uiProjects: CardData[] = [
+  { image: placeholder, title: 'Card 1', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 2', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 3', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 4', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'},
+  { image: placeholder, title: 'Card 5', subtitle: 'Lorem ipsum dolor sit amet...', buttonText: 'Button'}
+]
+
+function renderCard ({ image, title, subtitle, buttonText }: CardData): string {
+  return `
+    <div class="card">
+        <img src=${image}>
+        <span class="title">${title}</span>
+        <span class="subtitle">${subtitle}</span>
+        <button class="link">${buttonText}</button>
+      </div>
+  `
+}
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="header">
     <h1>bearbau</h1>
@@ -11,32 +48,28 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       </ul>
   </div>
 
+  <div class="container">
+    <img src="${placeholder}" style="width: 200px;">
+    <h1>notebook: stats for nerds</h1>
+    <p>description blah blah</p>
+    <button>this is a button</button>
+  </div>
+
   <div class="container">   
     <h1>Certifications</h1>
-
     <div class="carousel">
-
-      <div class="card">
-        <img src=${placeholder}>
-        <span class="title">Card 1</span>
-        <span class="subtitle">Lorem ipsum dolor sit amet...</span>
-        <button class="link">Button</button>
-      </div>
-
-      <div class="card">
-        <img src=${placeholder}>
-        <span class="title">Card 2</span>
-        <span class="subtitle">Lorem ipsum dolor sit amet...</span>
-        <button class="link">Button</button>
-      </div>
-
-      <div class="card">
-        <img src=${placeholder}>
-        <span class="title">Card 3</span>
-        <span class="subtitle">Lorem ipsum dolor sit amet...</span>
-        <button class="link">Button</button>
-      </div>
-
+      ${certifications.map(renderCard).join('')}
     </div>
+  </div>
+
+  <div class="container">
+    <h1>UI Projects</h1>
+    <div class="carousel">
+      ${uiProjects.map(renderCard).join('')}
+    </div>
+  </div>
+
+  <div class="footer">
+    <span class="subtitle">made with <3 on react</span>
   </div>
 `
