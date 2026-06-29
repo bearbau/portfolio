24fb197e0a4627f86998e5ea8342bb6cede78b19
bearbau <beatrizmarie.bautista@gmail.com>
2026-06-29 12:28:21 +0800

    hero page redo

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { Routes, Route } from "react-router";
 import Home from "./routes/Home";
 import Projects from "./routes/Projects";
 import Blog from "./routes/Blog";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <div className="app">
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,42 @@
 import Skillset from "./Skillset";
+import Header from "./Header";
 
 export default function Hero() {
   return (
     <>
-      <div className="hero-top">
-        <div className="hero-spacer"></div>
-        <div className="hero-content">
-          <span className="hero-title">
-            I'm Beatriz Bautista.&nbsp;
-            <i className="fa-solid fa-fish-fins"></i>
-          </span>
+      <div className="hero">
+        <Header />
+        <div className="hero-top">
+          <div className="hero-spacer"></div>
+          <div className="hero-content">
+            <p>hi there.</p>
+            <span className="hero-title">
+              I'm Beatriz Bautista.&nbsp;
+              <i className="fa-solid fa-fish-fins"></i>
+            </span>
 
-          <p>front-end dev & UI/UX designer.</p>
-          <div className="btn-row">
-            <button className="link">
-              <i className="fa-regular fa-file">&nbsp;</i>resume
-            </button>
-            <button className="link-circle">
-              <i className="fa-brands fa-github"></i>
-            </button>
-            <button className="link-circle">
-              <i className="fa-brands fa-telegram"></i>
-            </button>
-            <button className="link-circle">
-              <i className="fa-solid fa-keyboard"></i>
-            </button>
-            <button className="link-circle">
-              <i className="fa-solid fa-flag"></i>
-            </button>
+            <p>front-end dev & UI/UX designer.</p>
+            <div className="btn-row">
+              <button className="hero-btn">
+                <i className="fa-regular fa-file">&nbsp;</i>resume
+              </button>
+              <button className="hero-btn-circle">
+                <i className="fa-brands fa-github"></i>
+              </button>
+              <button className="hero-btn-circle">
+                <i className="fa-brands fa-telegram"></i>
+              </button>
+              <button className="hero-btn-circle">
+                <i className="fa-solid fa-keyboard"></i>
+              </button>
+              <button className="hero-btn-circle">
+                <i className="fa-solid fa-flag"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
       <Skillset />
     </>
   );
