7ae1a52e18b2227b34487a5af2d235a332ce72dd
bearbau <beatrizmarie.bautista@gmail.com>
2026-06-30 10:07:20 +0800

    responsive skillset

--- src/components/Skillset.tsx
+++ src/components/Skillset.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import placeholder from "../assets/hero-wallpaper.jpg";
 
 function Skillset() {
   const [activeView, setActiveView] = useState("about-hero");
@@ -7,120 +6,25 @@ function Skillset() {
   return (
     <>
       <div className="cntr-row">
-        <div
-          className="col"
-          style={{ flex: "50%", height: "50vh", paddingRight: "1rem" }}
-        >
-          <h1>What I do</h1>
-          <p>
-            I am a front-end web developer and UI/UX designer who has been
-            developing and conceptualizing web designs since 2020. <br />
-            <br />
-            Currently pursuing a degree in Computer Science, I also spend my
-            free time studying cybersecurity, solving CTF challenges, and
-            writing Bash scripts.
-          </p>
-          <br />
-          <div className="stack-row">
-            <button className="stack">
-              <i className="fa-brands fa-html5"></i>&nbsp;html
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-css"></i>&nbsp;css
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-bootstrap"></i>&nbsp;bootstrap
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-react"></i>&nbsp;react
-            </button>{" "}
-            <button className="stack">
-              <i className="fa-brands fa-js"></i>&nbsp;javascript
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-typescript"></i>&nbsp;typescript
-            </button>
-            <button className="stack">
-              <i className="fa-solid fa-bolt-lightning"></i>&nbsp;vite
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-figma"></i>&nbsp;figma
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-java"></i>&nbsp;java
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-python"></i>&nbsp;python
-            </button>
-            <button className="stack">
-              <i className="fa-solid fa-database"></i>&nbsp;SQL
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-linux"></i>&nbsp;linux
-            </button>
-            <button className="stack">
-              <i className="fa-solid fa-terminal"></i>&nbsp;bash
-            </button>
-            <button className="stack">
-              <i className="fa-brands fa-docker"></i>&nbsp;docker
-            </button>
-          </div>
-        </div>
-        <div
-          className="col"
-          style={{ flex: "50%", flexDirection: "row", padding: "0px" }}
-        >
-          <div className="oval">
-            <p>front-end</p>
-          </div>
-          <div className="oval">
-            <p>back-end</p>
-          </div>
-          <div className="oval">
-            <p>shell-scripting</p>
-          </div>
-        </div>
-      </div>
-
-      {/*<div className="hero-bottom">
-        <div className="nav-container">
-          <div
-            className={`nav-col ${activeView === "front-end-hero" ? "nav-col--active" : ""}`}
-            onClick={() => setActiveView("front-end-hero")}
-          >
-            <i className="fa-solid fa-caret-down"></i>&nbsp;Front-end
-          </div>
-          <div
-            className={`nav-col ${activeView === "pl-hero" ? "nav-col--active" : ""}`}
-            onClick={() => setActiveView("pl-hero")}
-          >
-            <i className="fa-solid fa-caret-down"></i>&nbsp;Back-end
-          </div>
-          <div
-            className={`nav-col ${activeView === "bash-hero" ? "nav-col--active" : ""}`}
-            onClick={() => setActiveView("bash-hero")}
-          >
-            <i className="fa-solid fa-caret-down"></i>&nbsp;Shell Scripting
-          </div>
-        </div>
-        {activeView !== "about-hero" && (
-          <button
-            className="link-circle"
-            onClick={() => setActiveView("about-hero")}
-          >
-            <i className="fa-solid fa-house"></i>
-          </button>
-        )}
-
         {activeView === "about-hero" && (
-          <div className="hero-info" style={{ padding: "2rem 2rem 0rem 2rem" }}>
-            <h1 style={{ fontSize: "2rem" }}>What I do</h1>
+          <div
+            className="col"
+            style={{
+              flex: "50%",
+              height: "50vh",
+              paddingRight: "1rem",
+              animation: "fadeIn 0.7s ease",
+            }}
+          >
+            <h1>What I do</h1>
             <p>
-              I am a <u>front-end web developer and UI/UX designer</u> currently
-              studying Computer Science.
-              On the side, I study cybersec, solve CTFs, and write Bash scripts.
+              I am a front-end web developer and UI/UX designer who has been
+              developing and conceptualizing web designs since 2020. <br />
+              <br />
+              Currently pursuing a degree in Computer Science, I also spend my
+              free time studying cybersecurity, solving CTF challenges, and
+              writing Bash scripts.
             </p>
-
             <br />
             <div className="stack-row">
               <button className="stack">
@@ -147,56 +51,39 @@ function Skillset() {
               <button className="stack">
                 <i className="fa-brands fa-figma"></i>&nbsp;figma
               </button>
-              <button
-                className="stack"
-                style={{ backgroundColor: "var(--primary-darker)" }}
-              >
+              <button className="stack">
                 <i className="fa-brands fa-java"></i>&nbsp;java
               </button>
-              <button
-                className="stack"
-                style={{ backgroundColor: "var(--primary-darker)" }}
-              >
+              <button className="stack">
                 <i className="fa-brands fa-python"></i>&nbsp;python
               </button>
-              <button
-                className="stack"
-                style={{ backgroundColor: "var(--primary-darker)" }}
-              >
+              <button className="stack">
                 <i className="fa-solid fa-database"></i>&nbsp;SQL
               </button>
-              <button
-                className="stack"
-                style={{
-                  backgroundColor: "var(--primary-muted)",
-                }}
-              >
+              <button className="stack">
                 <i className="fa-brands fa-linux"></i>&nbsp;linux
               </button>
-              <button
-                className="stack"
-                style={{
-                  backgroundColor: "var(--primary-muted)",
-                }}
-              >
+              <button className="stack">
                 <i className="fa-solid fa-terminal"></i>&nbsp;bash
               </button>
-              <button
-                className="stack"
-                style={{
-                  backgroundColor: "var(--primary-muted)",
-                }}
-              >
+              <button className="stack">
                 <i className="fa-brands fa-docker"></i>&nbsp;docker
               </button>
             </div>
-            <br />
           </div>
         )}
 
         {activeView === "front-end-hero" && (
-          <div className="hero-info">
-            <h1 style={{ fontSize: "2rem" }}>Front-end</h1>
+          <div
+            className="col"
+            style={{
+              flex: "50%",
+              height: "50vh",
+              paddingRight: "1rem",
+              animation: "fadeIn 0.7s ease",
+            }}
+          >
+            <h1>Front-end</h1>
             <p>
               I discovered my passion in front-end back in 2020 when I was
               tinkering with Bootstrap on a website. Nowadays, I use React and
@@ -229,41 +116,62 @@ function Skillset() {
                 <i className="fa-brands fa-figma"></i>&nbsp;figma
               </button>
             </div>
+            <button
+              className="link-circle"
+              onClick={() => setActiveView("about-hero")}
+            >
+              <i className="fa-solid fa-house"></i>
+            </button>
           </div>
         )}
+
         {activeView === "pl-hero" && (
-          <div className="hero-info">
-            <h1 style={{ fontSize: "2rem" }}>Back-end</h1>
+          <div
+            className="col"
+            style={{
+              flex: "50%",
+              height: "50vh",
+              paddingRight: "1rem",
+              animation: "fadeIn 0.7s ease",
+            }}
+          >
+            <h1>Back-end</h1>
             <p>
               These are the languages I picked up through my Computer Science
-              degree (especially with Java).
+              degree. I mostly use Java to code.
             </p>
             <br />
             <div className="stack-row">
-              <button
-                className="stack"
-                style={{ backgroundColor: "var(--primary-darker)" }}
-              >
+              <button className="stack">
                 <i className="fa-brands fa-java"></i>&nbsp;java
               </button>
-              <button
-                className="stack"
-                style={{ backgroundColor: "var(--primary-darker)" }}
-              >
+              <button className="stack">
                 <i className="fa-brands fa-python"></i>&nbsp;python
               </button>
-              <button
-                className="stack"
-                style={{ backgroundColor: "var(--primary-darker)" }}
-              >
+              <button className="stack">
                 <i className="fa-solid fa-database"></i>&nbsp;SQL
               </button>
             </div>
+            <button
+              className="link-circle"
+              onClick={() => setActiveView("about-hero")}
+            >
+              <i className="fa-solid fa-house"></i>
+            </button>
           </div>
         )}
-        {activeView === "bash-hero" && (
-          <div className="hero-info">
-            <h1 style={{ fontSize: "2rem" }}>Shell Scripting</h1>
+
+        {activeView === "sh-hero" && (
+          <div
+            className="col"
+            style={{
+              flex: "50%",
+              height: "50vh",
+              paddingRight: "1rem",
+              animation: "fadeIn 0.7s ease",
+            }}
+          >
+            <h1>Shell scripting</h1>
             <p>
               In my free time, I solve CTFs and study Bash scripting, but I
               mostly study it for the love of the game. I dump my Bash scripts
@@ -275,34 +183,49 @@ function Skillset() {
             </p>
             <br />
             <div className="stack-row">
-              <button
-                className="stack"
-                style={{
-                  backgroundColor: "var(--primary-muted)",
-                }}
-              >
-                <i className="fa-brands fa-linux"></i>&nbsp;linux
+              <button className="stack">
+                <i className="fa-brands fa-java"></i>&nbsp;java
               </button>
-              <button
-                className="stack"
-                style={{
-                  backgroundColor: "var(--primary-muted)",
-                }}
-              >
-                <i className="fa-solid fa-terminal"></i>&nbsp;bash
+              <button className="stack">
+                <i className="fa-brands fa-python"></i>&nbsp;python
               </button>
-              <button
-                className="stack"
-                style={{
-                  backgroundColor: "var(--primary-muted)",
-                }}
-              >
-                <i className="fa-brands fa-docker"></i>&nbsp;docker
+              <button className="stack">
+                <i className="fa-solid fa-database"></i>&nbsp;SQL
               </button>
             </div>
+            <button
+              className="link-circle"
+              onClick={() => setActiveView("about-hero")}
+            >
+              <i className="fa-solid fa-house"></i>
+            </button>
           </div>
         )}
-      </div> */}
+
+        <div
+          className="col"
+          style={{ flex: "50%", flexDirection: "row", padding: "0px" }}
+        >
+          <div
+            className={`oval ${activeView === "front-end-hero" ? "oval--active" : ""}`}
+            onClick={() => setActiveView("front-end-hero")}
+          >
+            <p>front-end</p>
+          </div>
+          <div
+            className={`oval ${activeView === "pl-hero" ? "oval--active" : ""}`}
+            onClick={() => setActiveView("pl-hero")}
+          >
+            <p>back-end</p>
+          </div>
+          <div
+            className={`oval ${activeView === "sh-hero" ? "oval--active" : ""}`}
+            onClick={() => setActiveView("sh-hero")}
+          >
+            <p>shell-scripting</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
